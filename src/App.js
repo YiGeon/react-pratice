912738da583a8ad4572bd84f7b9bb10617c91c66
YiGeon Park <parkegun@gmail.com>
2021-06-27 00:03:13 +0900

    add counter modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import CounterContainer from "./containers/CounterContainer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <CounterContainer/>
+        </div>
+    );
 }
 
 export default App;
